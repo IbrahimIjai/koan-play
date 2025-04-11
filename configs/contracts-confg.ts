e8b631f76381e210c0fb6f6cca49023701dfd52e
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2025-04-11 20:41:52 +0100

    contracts

---
+++ configs/contracts-confg.ts
@@ -0,0 +1,11 @@
+import { base, baseSepolia } from "viem/chains";
+
+export const RANDOMNUMBER_GENERATOR = {
+  [baseSepolia.id]: "0x9A8A568891031EEF8df7A8dC8976b976ad2243D3",
+  //   [base.id]: "0x9A8A568891031EEF8df7A8dC8976b976ad2243D3",
+};
+
+export const KOAN_LOTTERY_CONTRACT = {
+  [baseSepolia.id]: "0x6BA6E5d63dc7B87b8D3E13627221aEE83e0165c1",
+  //   [base.id]: "0x9A8A568891031EEF8df7A8dC8976b976ad2243D3",
+};
--- configs/reown.ts
+++ configs/reown.ts
@@ -1,10 +1,10 @@
 import { cookieStorage, createStorage } from "@wagmi/core";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { base } from "@reown/appkit/networks";
+import { base, baseSepolia } from "@reown/appkit/networks";
 // import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 export const projectId = "926aea17b9e7bceeaff476f56a0d1d95";
 
-export const networks = [base];
+export const networks = [base, baseSepolia];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
