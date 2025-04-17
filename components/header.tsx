902866dc6c6142a44c237a3869185820ab007881
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2025-04-17 03:01:00 +0100

    testing header 1.

--- components/header.tsx
+++ components/header.tsx
@@ -3,13 +3,31 @@
 import React from "react";
 import ConnectButton from "./connect-button";
 import { useMiniKit } from "@/hooks/useMiniKit";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function Header() {
   const { context } = useMiniKit();
 
   console.log({ context });
   return (
-    <div>
+    <div className="flex items-center justify-between px-3 py-2">
+      <div className="flex items-center gap-1">
+        <Avatar className="h-4 w-4">
+          <AvatarImage
+            src={context?.user.pfpUrl}
+            alt={context?.user.displayName}
+          />
+          <AvatarFallback>
+            {context?.user?.displayName
+              ? context?.user?.displayName.slice(0, 1)
+              : "KP"}
+          </AvatarFallback>
+        </Avatar>
+        <p className="text-xs text-muted-foreground">
+          {context?.user?.displayName}
+        </p>
+      </div>
+
       <ConnectButton />
     </div>
   );
